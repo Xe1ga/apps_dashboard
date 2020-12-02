d984207e74115cf93ecf33a1a9951159c5518dd4
Xe1ga <frenzied89@mail.ru>
2020-12-02 14:17:44 +0300

    Редактирование имени переменной окружения

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from structure import ReportParams
 
 def run():
     report_params = ReportParams(
-        username=env.str('USERNAMEAF'),
+        username=env.str('USERNAME'),
         password=env.str('PASSWORD'),
         app_key=env.str('APP_KEY'),
         base_url=env.str('BASE_URL'),
